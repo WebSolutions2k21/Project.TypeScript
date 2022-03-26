a4334ae631df04d3e2f560086cefbc2ec4e65f67
SolWika <solwika@github.com>
2022-03-26 23:57:50 +0100

    prepare an i18n.js file, add to App.tsx
    add Language Switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from "react";
 
 import { RegistrationForm } from './components/Registration'
+import { useTranslation, Trans } from "react-i18next";
+
+const lngs = {
+  en: { nativeName: "English" },
+  pl: { nativeName: "Polish" },
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div>
       hello!
       <RegistrationForm />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              key={lng}
+              style={{
+                fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+              }}
+              type="submit"
+              onClick={() => i18n.changeLanguage(lng)}
+            >
+              {lng}
+            </button>
+          ))}
+        </div>
+          <Trans i18nKey={"description.part1"}>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </Trans>
+          {t("description.part2")}
     </div>
   );
 }
