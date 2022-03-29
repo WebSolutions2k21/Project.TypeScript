e00025749e279c72ab9ee5239341dce86a4ffdb7
SolWika <solwika@github.com>
2022-03-29 18:24:31 +0200

    without {" "}

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,35 +13,32 @@ function Footer() {
   const [count, setCounter] = useState(0);
   return (
     <div>
-      {" "}
-      <header className="App-header">
-        <div>
-          {Object.keys(lngs).map((lng) => (
-            <button
-              key={lng}
-              style={{
-                fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
-              }}
-              type="submit"
-              onClick={() => {
-                i18n.changeLanguage(lng);
-                setCounter(count + 1);
-              }}
-            >
-              {lng}
-            </button>
-          ))}
-        </div>
-        <p>
-          <Trans i18nKey={"description.part1"}>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </Trans>
-        </p>
-        <i>{t("counter", { count })}</i>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          {t("description.part2")}
-        </a>
-      </header>
+      <div>
+        {Object.keys(lngs).map((lng) => (
+          <button
+            key={lng}
+            style={{
+              fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+            }}
+            type="submit"
+            onClick={() => {
+              i18n.changeLanguage(lng);
+              setCounter(count + 1);
+            }}
+          >
+            {lng}
+          </button>
+        ))}
+      </div>
+      <p>
+        <Trans i18nKey={"description.part1"}>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </Trans>
+      </p>
+      <i>{t("counter", { count })}</i>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        {t("description.part2")}
+      </a>
       {t("footer.date", { date: new Date(), context: getGreetingTime() })}
     </div>
   );
