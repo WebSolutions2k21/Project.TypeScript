d480f81d7b5b4b8817fd5589797b1ada8152c871
kfilip <katarzyna.filip87@gmail.com>
2022-04-24 21:05:53 +0200

    index fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,8 @@
 export * from "./Registration";
 export * from "./Login";
 export * from "./ForgotPassword";
-<<<<<<< HEAD
 export * from "./Profile";
-=======
 export * from "./AllTeamProject";
 export * from "./Modal";
->>>>>>> 40d1be6ae1c14c2d9c66634e5ab7bf1bafc1d271
 export * from "./AddNewProject";
 export * from "./Navbar";
