d3df1f3058ff9ce5d2e0ec13b34bd27371e5435f
Aleksander <aleksander.atamanczuk@gmail.com>
2022-05-24 23:10:35 +0200

    refactor

--- src/pages/MentorProfilePage.tsx
+++ src/pages/MentorProfilePage.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Profile } from "components";
 import { Navbar } from "components";
-import { useTranslation } from "react-i18next";
 
 export const MentorProfilePage = () => {
-  const { t } = useTranslation();
   return (
     <>
-        <Navbar />
-        <Profile/>
-        </>
-);
+      <Navbar />
+      <Profile />
+    </>
+  );
 };
